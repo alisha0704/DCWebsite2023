e7f4d7717860300204226a79e925d0d65f3a640a
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-08-17 16:35:41 +0530

    feat: added info and fests section

--- src/components/uncommon/Slideshow.tsx
+++ src/components/uncommon/Slideshow.tsx
@@ -56,6 +56,35 @@ const Slideshow:React.FC<SlideshowProps> = () => {
         </div>
       </div>
     </div>
+    <div className='w-1/2 mx-auto relative bg-black pt-20 px-20'>
+        <h1 className='text-white text-4xl'>VIT Dance Club promote the dance art form both <p><span style={{ color: '#3947E2' }}>regional</span> and <span style={{ color: '#3947E2' }}>western</span> and give opportunity for</p> students to perform</h1>
+        <h5 className='text-gray-400 text-sm pt-4'>Winners of various dance competitions across the country & club consists of seven major teams of varied dance forms across India.</h5>
+    </div>
+    <div className='py-10'>
+    <div className='flex justify-center items-center mt-8 space-x-6'>
+    <Image src='/flaglogo.png' alt='Logo 1' width={40} height={40} style={{ marginRight: '1rem' }} />
+    <Image src='/7.png' alt='Logo 2' width={15} height={15} style={{ marginRight: '4rem' }}/>
+    <Image src='/humanlogo.png' alt='Logo 3' width={40} height={40} />
+    <Image src='/150+.png' alt='Logo 4' width={50} height={50}  style={{ marginRight: '4rem' }}/>
+    <Image src='/taglogo.png' alt='Logo 4' width={50} height={50} />
+    <Image src='/200+.png' alt='Logo 5' width={50} height={50} />
+    </div>
+
+        <div className='flex justify-center items-center space-x-16 w-full' style={{ marginTop: '-0.5rem' }} >
+        <Image src='/majorteams.png' alt='Logo 6' width={70} height={70} />
+        <div className='flex justify-center items-center space-x-10'>
+            <Image src='/crewmembers.png' alt='Logo 7' width={70} height={70} style={{ marginRight: '1rem' }}/>
+            <Image src='/performances.png' alt='Logo 8' width={70} height={70} />
+    </div>
+    </div>
+    </div>
+    <div className='text-center mt-16 py-6'>
+    <h2 className='text-sm text-gray-400'>MAJOR PRIZE WINNING STAGES</h2>
+    <div className='flex justify-center items-center mt-4'>
+    <Image src='/festlogo.png' alt='fests won' width={800} height={200} />
+    </div>
+    </div>
+
     </div>
     )
 }
