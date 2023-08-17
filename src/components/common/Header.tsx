4ee09d5360a4b4e705f96eabb78c36f13fa216ce
anubhav-aryan <aryananubhav08@gmail.com>
2023-08-17 18:39:06 +0530

    Fix : Header UI

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,110 +10,41 @@ const Header = () => {
   };
 
   return (
-    <div className="w-full bg-black pt-4">
-      <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-        <nav
-          className={`flex-col flex-grow pb-4 md:pb-0 ${
-            menuOpen ? "flex" : "hidden"
-          } md:flex md:justify-center md:flex-row`}
-        >
-          <div className="relative">
-            <button
-              onClick={toggleMenu}
-              className="flex flex-row items-center w-full px-4 py-2 mt-2 text-2xl   font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 focus:outline-none focus:shadow-outline"
-            >
-              <span className="text-white">Dropdown</span>
-              <svg
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                className={`inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform ${
-                  menuOpen ? "rotate-180" : "rotate-0"
-                }`}
-              >
-                <path fill-rule="evenodd" d="..." />
-              </svg>
-            </button>
-            <div
-              className={`absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48 ${
-                menuOpen ? "animate-slide-down" : "hidden"
-              }`}
-            >
-              <div className="px-2 py-2 bg-black rounded-md shadow">
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                  THE BOARD 2023
-                </a>
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                 FACULTY COORDINATORS  
-                </a>
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                  DEVELOPER TEAM 
-                </a>
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                  BOARD HISTORY
-                </a>
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                  JOURNEY OF DC 
-                </a>
-                <a
-                  className="block px-4 py-2 mt-2 text-sm font-semibold text-white bg-transparent rounded-lg md:mt-0 focus:outline-none focus:shadow-outline"
-                  href="#"
-                >
-                  NOTABLE ALUMNIS  
-                </a>
-              </div>
-            </div>
+    <header>
+      <nav className="w-full bg-gray-800 text-white pt-4 pb-6">
+        <div className="container mx-auto flex flex-row justify-center p-2">
+          <div className="pr-32">
+            <Link href="/">
+              <Image
+                src="/black n white logo 2.svg"
+                alt="DC"
+                width={50}
+                height={50}
+              />
+            </Link>
           </div>
-
-          {/* Dropdown ends and navbar starts */}
-
-          <a
-            className="text-white px-4 py-2 mt-2 text-2xl font-semibold rounded-lg md:mt-0 transition duration-200 hover:text-white"
-            href="#"
-          >
-            HOME
-          </a>
-          <a
-            className="text-white px-4 py-2 mt-2 text-2xl font-semibold rounded-lg md:mt-0 transition duration-200 hover:text-white"
-            href="#"
-          >
-            GALLERY
-          </a>
-          <a
-            className="text-white px-4 py-2 mt-2 text-2xl font-semibold rounded-lg md:mt-0 transition duration-200 hover:text-white"
-            href="#"
-          >
-            EVENTS
-          </a>
-          <a
-            className="text-white px-4 py-2 mt-2 text-2xl font-semibold rounded-lg md:mt-0 transition duration-200 hover:text-white"
-            href="#"
-          >
-            ACHIEVEMENTS
-          </a>
-          <a
-            className="text-white px-4 py-2 mt-2 text-2xl font-semibold rounded-lg md:mt-0 transition duration-200 hover:text-white"
-            href="#"
-          >
-            CONTACT US
-          </a>
-        </nav>
-      </div>
-    </div>
+          <ul className="flex flex-row mt-4 text-white gap-20 text-xl">
+            <ul className="flex flex-row mt-4 text-white gap-20 text-xl">
+              <li>
+                <Link href="/">HOME</Link>
+              </li>
+              <li>
+                <Link href="/Gallery">GALLERY</Link>
+              </li>
+              <li>
+                <Link href="/Events">EVENTS</Link>
+              </li>
+              <li>
+                <Link href="/Achievements">ACHIEVEMENTS</Link>
+              </li>
+              <li>
+                <Link href="/ContactUs">CONTACT US</Link>
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- src/components/common/Leftsidebar.tsx
+++ src/components/common/Leftsidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Leftsidebar = () => {
   return (
-    <div className="bg-black relative h-screen w-32">
+    <div className="bg-red-400 relative h-screen w-32">
       <div className="absolute inset-y-0 left-0 w-16"></div>
     </div>
   );
