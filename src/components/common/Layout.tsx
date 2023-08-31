f04bcbfebcf7468a70c7fea4e1ec6f8c31055501
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-01 02:44:47 +0530

    fix: loader

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,15 +1,28 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Header from "./Header";
 import Leftsidebar from "./Leftsidebar";
 import Rightsidebar from "./Rightsidebar";
+import FullpageLoader from "./FullpageLoader";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setLoading(false), 5000);
+
+    return () => {
+      clearTimeout(timeoutId);
+      sessionStorage.removeItem("scrollPosition");
+    };
+  }, []);
+
   return (
     <>
+      <FullpageLoader loading={loading} />
       <Header />
       <div className="absolute inset-y-0 left-0 w-32">
         <Leftsidebar />
