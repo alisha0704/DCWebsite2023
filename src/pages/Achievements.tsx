8681384e565d23134e5c046d5f85574ab7c18467
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-13 11:39:17 +0530

    feat: back arrow

--- src/pages/Achievements.tsx
+++ src/pages/Achievements.tsx
@@ -58,6 +58,25 @@ const Achievements = () => {
                 Our Achievements
               </h1>
             </div>
+            <Link
+              href="/"
+              className="absolute hidden lg:block top-16 left-14 text-white mt-20 ml-8 px-32 pt-8"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </Link>
             <p className="text-sm md:text-md lg:text-xl mb-8 text-gray-300">
               We never leave the stage empty-handed, either we win the
               audience&apos;s heart or we win the whole event!
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -79,6 +79,25 @@ const Board: React.FC<BoardProps> = () => {
                 The Board
               </h1>
             </div>
+            <Link
+              href="/"
+              className="absolute hidden lg:block top-16 left-14 text-white mt-20 ml-8 px-32 pt-8"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-12 h-12"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </Link>
             <p className="text-sm md:text-md mb-8 text-gray-300">
               Alone you can do so little, together you can do so much.
               Presenting the Board of Dance Club 2023-24.
