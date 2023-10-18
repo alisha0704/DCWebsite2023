036a4e71af9b2b2a3343405151890d45811bfee7
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-18 13:35:21 +0530

    feat: achievements

--- src/pages/Achievements.tsx
+++ src/pages/Achievements.tsx
@@ -1,11 +1,375 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { FiArrowLeft } from "react-icons/fi";
-import Image from "next/image";
-import FullpageLoader from "@/components/common/FullpageLoader";
-import Footer from "@/components/common/Footer";
+import React, { useState, useEffect } from "react";
+import Image from 'next/image'
+import Link from 'next/link'
+import { type } from 'os'
 
-const Achievements = () => {
+type AchievementsProps = {}
+
+const Achievements: React.FC<AchievementsProps> = () => {
+  const images = [
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/srishti.svg",
+      memberName: "Srishti Sinha",
+      memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aman.svg",
+      memberName: "Aman Bohara",
+      memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/gowri.svg",
+      memberName: "Gowri Namboodiri",
+      memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/manasa.png",
+      memberName: "Manasa Madhusoodanan",
+      memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/sree.svg",
+      memberName: "Sree Nandhan",
+      memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/teerth.svg",
+      memberName: "Teerth Amin",
+      memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/aditi.svg",
+      memberName: "Aditi Sudhir",
+      memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/nandin.svg",
+      memberName: "Nandin Sajith",
+      memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/board/raaga.svg",
+      memberName: "Raaga Laasya",
+      memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
+    },
+    {
+      imageUrl: "/achievements/image 55.svg",
+      memberName: "IIM Bangalore- 2017",
+      memberDesgn: "Footloose- 2nd place",
+      memberTeam: "Mirage",
+    },
+    
+  ]
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,52 +379,13 @@ const Achievements = () => {
     };
   }, []);
 
-  const images = [
-    {
-      imageUrl: "/achievements/achievement1.svg",
-      memberName: "SYNAPSE - 2023",
-      memberDesgn: "DUET DANCE- 1ST PLACE & 2ND PLACE CHOREONITE- 1ST PLACE",
-    },
-    {
-      imageUrl: "/achievements/achievement2.svg",
-      memberName: "VIBRANCE - 2023",
-      memberDesgn: "THEMATIC CATEGORY- WINNERS MAYA",
-    },
-    {
-      imageUrl: "/achievements/achievement3.svg",
-      memberName: "MOOD INDIGO - 2022",
-      memberDesgn: "THEMATIC CATEGORY- 1ST RUNNER UP MIRAGE",
-    },
-    {
-      imageUrl: "/achievements/achievement4.svg",
-      memberName: "SYNAPSE - 2023",
-      memberDesgn: "DUET DANCE- 1ST PLACE & 2ND PLACE CHOREONITE- 1ST PLACE",
-    },
-    {
-      imageUrl: "/achievements/achievement5.svg",
-      memberName: "VIBRANCE - 2023",
-      memberDesgn: "THEMATIC CATEGORY- WINNERS MAYA",
-    },
-    {
-      imageUrl: "/achievements/achievement6.svg",
-      memberName: "MOOD INDIGO - 2022",
-      memberDesgn: "THEMATIC CATEGORY- 1ST RUNNER UP UNITRIX",
-    },
-  ];
-
+  
   return (
     <>
-      <div>
-        <div className="flex justify-center items-center h-3/4 ">
-          <div className="text-center p-8 overflow-y-auto">
-            <div className="mb-4">
-              <h1 className="text-xl md:text-2xl lg:text-3xl mb-1 mt-8 text-white">
-                Our Achievements
-              </h1>
-            </div>
-            <Link
+      <div className="container">
+      <Link
               href="/"
-              className="absolute hidden lg:block top-16 left-14 text-white mt-20 ml-8 px-32 pt-8"
+              className="absolute hidden lg:block pt-14 text-white"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -77,11 +402,18 @@ const Achievements = () => {
                 />
               </svg>
             </Link>
-            <p className="text-sm md:text-md lg:text-xl mb-8 text-gray-300">
-              We never leave the stage empty-handed, either we win the
-              audience&apos;s heart or we win the whole event!
+        <div className="flex justify-center items-center   bg-black">
+          <div className="text-center p-8 max-w-screen-lg w-full overflow-y-auto">
+            <div className="mb-4">
+              <div className="text-2xl md:text-3xl mb-1 mt-8 text-white">
+                Our Achievements 
+              </div>
+            </div>
+            
+            <p className="text-sm md:text-md mb-8 text-gray-300">
+              We never left the stage empty handed, either we win the audience heart of we win the whole event 
             </p>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-10 py-8 w-full">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-14 py-8 w-full">
               {images.map((image, index) => (
                 <div key={index} className="text-center">
                   <Image
@@ -91,11 +423,14 @@ const Achievements = () => {
                     height={1000}
                   />
                   <div className="flex flex-col items-center mt-2">
-                    <h2 className="text-sm md:text-md lg:text-xl font-semibold text-white mt-2">
+                    <h2 className="text-md lg:text-lg font-semibold text-white mt-2">
                       {image.memberName}
                     </h2>
-                    <h3 className="text-xs text-gray-400 mt-1 mb-8">
+                    <h3 className="text-xs lg:text-sm text-gray-400 mt-1">
                       {image.memberDesgn}
+                    </h3>
+                    <h3 className="text-xs lg:text-sm text-gray-400 mb-8">
+                      {image.memberTeam}
                     </h3>
                   </div>
                 </div>
@@ -105,7 +440,7 @@ const Achievements = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Achievements;
+export default Achievements
