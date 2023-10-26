b1dd8526dfed06eefbce6e7da13c1965f644197d
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-26 17:33:59 +0530

    fix: events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -76,189 +76,262 @@ const Events = () => {
         workshops, where the magic of movement meets the wisdom of experts!
       </div>
       <div className="gap-8 columns-3 py-10">
-        <Image src="/events/gallery1/image 106.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
+        <Image
           src="/events/gallery1/image 106.svg"
-        /> */}
-        <Image src="/events/gallery1/image 25.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+
+        <Image
           src="/events/gallery1/image 25.svg"
-        /> */}
-        <Image src="/events/gallery1/image 109.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        {/* <img
-          className="w-full aspect-square mb-6"
+        <Image
           src="/events/gallery1/image 109.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 113.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
+        <Image
           src="/events/gallery1/image 113.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 108.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
+        <Image
           src="/events/gallery1/image 108.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 107.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
+        <Image
           src="/events/gallery1/image 107.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 111.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
+        <Image
           src="/events/gallery1/image 111.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 110.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
+        <Image
           src="/events/gallery1/image 110.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery1/image 112.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
+        <Image
           src="/events/gallery1/image 112.svg"
-        /> */}
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
       </div>
       <div className=" text-2xl py-10 font-semibold">
         ----------Alumni Events----------------------------------------
       </div>
       <div className="gap-4 columns-4 py-10">
-        <Image src="/events/gallery3/image 114.svg" alt="" height={1000} width={1000} className="w-full" />
-        <div className="text-center"><h1>GeethaNritya </h1> Alumni Geethanjali Ajith</div>
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 106.svg"
-        /> */}
-        <Image src="/events/gallery3/image 119.svg" alt="" height={1000} width={1000} className="w-full" />
-        <div className="text-center"><h1>Taal se taal Mila
-        </h1> Riwaayat alumni Meher Nigam</div>
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 25.svg"
-        /> */}
-        <div className="text-center  pt-10"><h1>Groove And Go </h1> Unitrix alumni Sarang Ram</div>
-        <Image src="/events/gallery3/image 121.svg" alt="" height={1000} width={1000} className="w-full" />
+        <Image
+          src="/events/gallery3/image 114.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
+        <div className="text-center">
+          <h1>GeethaNritya </h1> Alumni Geethanjali Ajith
+        </div>
+        <Image
+          src="/events/gallery3/image 119.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
+        <div className="text-center">
+          <h1>Taal se taal Mila</h1> Riwaayat alumni Meher Nigam
+        </div>
+        <div className="text-center  pt-10">
+          <h1>Groove And Go </h1> Unitrix alumni Sarang Ram
+        </div>
+        <Image
+          src="/events/gallery3/image 121.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
 
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 109.svg"
-        /> */}
+        <div className="text-center">
+          <h1>Flow with it </h1> Chargers alumni Gautham
+        </div>
+        <Image
+          src="/events/gallery3/image 120.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
 
-        <div className="text-center"><h1>Flow with it </h1> Chargers alumni Gautham</div>
-        <Image src="/events/gallery3/image 120.svg" alt="" height={1000} width={1000} className="w-full" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 113.svg"
-        /> */}
+        <Image
+          src="/events/gallery3/image 115.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
+        <div className="text-center">
+          <h1>Pump it Up </h1>JNJ Bhangra alumni Aniket
+        </div>
 
-        <Image src="/events/gallery3/image 115.svg" alt="" height={1000} width={1000} className="w-full" />
-        <div className="text-center"><h1>Pump it Up </h1>JNJ Bhangra alumni Aniket</div>
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 108.svg"
-        /> */}
+        <Image
+          src="/events/gallery3/image 118.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
+        <div className="text-center">
+          <h1>Noopura </h1>Maya alumni Sariga
+        </div>
 
-        <Image src="/events/gallery3/image 118.svg" alt="" height={1000} width={1000} className="w-full" />
-        <div className="text-center"><h1>Noopura </h1>Maya alumni Sariga</div>
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 107.svg"
-        /> */}
-        
-        <div className="text-center  pt-10"><h1>Lumiere </h1> Mirage alumni Shubham</div>
-        <Image src="/events/gallery3/image 117.svg" alt="" height={1000} width={1000} className="w-full" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 111.svg"
-        /> */}
-        <div className="text-center"><h1>Feel The Beat </h1> BBC alumni Aditya Roshan</div>
-        <Image src="/events/gallery3/image 116.svg" alt="" height={1000} width={1000} className="w-full" />
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 110.svg"
-        /> */}
+        <div className="text-center  pt-10">
+          <h1>Lumiere </h1> Mirage alumni Shubham
+        </div>
+        <Image
+          src="/events/gallery3/image 117.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
+        <div className="text-center">
+          <h1>Feel The Beat </h1> BBC alumni Aditya Roshan
+        </div>
+        <Image
+          src="/events/gallery3/image 116.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full"
+        />
       </div>
       <div className=" text-2xl py-10 font-semibold">
-        ----------Other Offline/Online Events----------------------------------------
+        ----------Other Offline/Online
+        Events----------------------------------------
       </div>
       <div className="gap-4 columns-4 py-10">
-        <Image src="/events/gallery2/image 24.svg" alt="" height={1000} width={1000} className="w-full mb-6 mt-16" />
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 106.svg"
-        /> */}
-        <Image src="/events/gallery2/image 124.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 25.svg"
-        /> */}
-        <Image src="/events/gallery2/image 135.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
+        <Image
+          src="/events/gallery2/image 24.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6 mt-16"
+        />
+        <Image
+          src="/events/gallery2/image 124.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/events/gallery2/image 135.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 109.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 122.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery2/image 122.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 113.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 125.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery2/image 125.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 108.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 132.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery2/image 132.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 107.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 27.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6 pt-10"
+        />
 
-        <Image src="/events/gallery2/image 27.svg" alt="" height={1000} width={1000} className="w-full mb-6 pt-10" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 111.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 26.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
 
-        <Image src="/events/gallery2/image 26.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-video mb-6"
-          src="/events/gallery1/image 110.svg"
-        /> */}
-
-        <Image src="/events/gallery2/image 133.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 112.svg"
-        /> */}
-         <Image src="/events/gallery2/image 123.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 112.svg"
-        /> */}
-         <Image src="/events/gallery2/image 28.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 112.svg"
-        /> */}
-         <Image src="/events/gallery2/image 134.svg" alt="" height={1000} width={1000} className="w-full mb-6" />
-        {/* <img
-          className="w-full aspect-square mb-6"
-          src="/events/gallery1/image 112.svg"
-        /> */}
+        <Image
+          src="/events/gallery2/image 133.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/events/gallery2/image 123.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/events/gallery2/image 28.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/events/gallery2/image 134.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
       </div>
     </>
   );
