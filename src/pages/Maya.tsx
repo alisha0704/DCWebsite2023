e83c356d3ae1094299d973ee62044eb87689f4cc
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-30 20:06:16 +0530

    feat: Maya page

---
+++ src/pages/Maya.tsx
@@ -0,0 +1,193 @@
+import React from "react";
+import Image from "next/image";
+
+const Maya = () => {
+  return (
+    <>
+      <div className="text-3xl text-center font-extrabold">MAYA</div>
+      <div className="text-center justify-center text-sm text-gray-400">
+        Maya, is the team that can effortlessly synchronize their movements to
+        any music because they find their true selves in classical dance. And
+        they are versatility of performing both soothing classical compositions
+        to ever-jamming pop hits with equal aplomb.
+      </div>
+      <hr className="my-5" />
+      <div>
+        <Image
+          src="/Maya/004A8099 2.svg"
+          alt="BBC"
+          width={500}
+          height={500}
+          className="w-full"
+        />
+      </div>
+      <div className="py-10 text-center justify-center flex border rounded-3xl mt-10">
+        <Image src="/BBC/Frame 11.svg" alt="BBC" width={40} height={40} />
+        <div className="px-5 py-2 font-bold text-xl">
+          {" "}
+          The Junior Dance Team of Maya is{" "}
+          <span className="text-[#BBC1FF]"> Laasya </span>{" "}
+        </div>
+      </div>
+      <div className="gap-4 columns-3 py-10">
+        <Image
+          src="/Maya/image 149.svg"
+          alt="BBC"
+          width={500}
+          height={500}
+          className="pt-12"
+        />
+        <div className="text-center pt-4 font-semibold">Arya S Raj</div>
+        <div className="text-center text-xs text-gray-400 font-semibold ">
+          TEAM REPRESENTATIVE
+        </div>
+        <Image src="/Maya/image 151.svg" alt="BBC" width={500} height={500} />
+        <div className="text-center pt-4 font-semibold">Shruti Suresh Nair</div>
+        <div className="text-center text-xs text-gray-400 font-semibold">
+          TEAM HEAD
+        </div>
+        <Image
+          src="/Maya/image 148.svg"
+          alt="BBC"
+          width={500}
+          height={500}
+          className="pt-12"
+        />
+        <div className="text-center pt-4 font-semibold">Shreya Mangesh</div>
+        <div className="text-center text-xs text-gray-400 font-semibold">
+          JDC COORDINATOR - LAASYA
+        </div>
+      </div>
+      <div className="text-3xl text-center font-extrabold">Mini-Gallery</div>
+      <div className="text-center justify-center text-sm text-gray-400">
+        Glimpse of our latest performances throughout many events
+      </div>
+      <div className="gap-4 columns-2 py-4">
+        <Image
+          src="/BBC/IMG-20230305-WA0029 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/BBC/004A7986 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+        <Image
+          src="/BBC/DSC_9641 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+
+        <Image
+          src="/BBC/bbc wdd 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+
+        <Image
+          src="/BBC/004A7993 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+
+        <Image
+          src="/BBC/DSC_9710 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6"
+        />
+
+        <Image
+          src="/BBC/IMG-20230616-WA0003 1.svg"
+          alt=""
+          height={1000}
+          width={1000}
+          className="w-full mb-6 pt-10"
+        />
+      </div>
+
+      <div className="lg:px-24 py-10 pb- flex-col flex">
+        <div className="text-center text-2xl md:text-3xl lg:text-4xl text-white font-medium">
+          Recent Performances
+        </div>
+        <div className="text-center text-neutral-400 pt-3 font-normal ">
+          We have performed in many different stages across India, also
+          conquered levels of awards too
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="/BBC/4 2.svg"
+            alt=""
+            height={150}
+            width={669}
+          />
+        </div>
+        <div className="px-10">
+          <div className="w-full h-6 text-white text-xl md:text-xl lg:text-xl font-semibold uppercase leading-normal tracking-wide pb-10">
+            World dance day, vit vellore
+          </div>
+
+          <div className="md:w-96 text-white text-sm sm:text-base font-normal leading-7 text-left justify-center items-center">
+            Experience a mesmerizing celebration of rhythm and culture paying
+            ode to World Dance Day with our dynamic performance.It is a night of
+            breathtaking choreography and boundless creativity as we pay homage
+            to the universal language of dance.
+          </div>
+          <div className="py-2"></div>
+          <div className="w-40 h-12 justify-center items-center ">
+            <div className="w-40 h-12 relative bg-white bg-opacity-10 rounded-sm flex-col justify-start items-start flex">
+              <div className="w-40 h-6 absolute">
+                <div className="w-40 h-6 left-0 top-2 absolute text-center text-white text-md md:text-xl font-normal leading-normal tracking-wide ">
+                  Watch Now
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="w-full h-6 px-6 text-white text-xl md:text-xl lg:text-xl font-semibold uppercase leading-normal tracking-wide pt-6 ">
+            international dance day, vit vellore
+          </div>
+          <div className="py-8"></div>
+          <div className="px-4 pl-6 text-white text-sm text-left pb-4">
+            Missed our International dance day performance? Don&apos;t sweat it
+            cause we&apos;ve bought international dance day TO YOU! <br />{" "}
+            Presenting a plethora of performances by our very own dance teams.
+            click below & enjoy the performance.
+          </div>
+          <div className="w-40 h-12  max-sm:px-10 justify-center items-center inline-flex">
+            <div className="w-40 h-12 relative bg-white bg-opacity-10 rounded-sm flex-col justify-start items-start flex">
+              <div className="w-40 h-6 relative">
+                <div className="w-40 h-6 top-2 absolute text-center text-white text-md md:text-xl font-normal leading-normal tracking-wide">
+                  Watch Now
+                </div>
+              </div>
+              <div className="w-5 h-5 origin-top-left rotate-[-34.72deg] bg-gradient-to-b from-emerald-300 to-green-600 rounded-full blur-xl" />
+              <div className="w-6 h-7 origin-top-left rotate-[-34.72deg] bg-gradient-to-b from-pink-400 to-violet-700 rounded-full blur-xl" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Image src="/BBC/image 156.svg" height={150} width={669} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Maya;
