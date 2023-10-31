c61c6fa5e501d21f4778ec3efb098fbef1bc8ab9
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-10-31 12:59:34 +0530

    fix: chargers team page

--- src/pages/chargers.tsx
+++ src/pages/chargers.tsx
@@ -75,13 +75,83 @@ const chargers:React.FC<chargersProps> = () => {
       <div className="text-center justify-center text-sm text-gray-400">
         Glimpse of our latest performances throughout many events
       </div>
-      <div className="gap-4 columns-2 py-4">
+      <div className="flex justify-center py-4">
         <Image src="/Chargers/chargersgallery.svg" 
         alt="Chargers"
-         width={500} 
-         height={500} />
+         width={1000} 
+         height={1000} />
       </div>
+      <div className="lg:px-24 py-10 pb- flex-col flex">
+        <div className="text-center text-2xl md:text-3xl lg:text-4xl text-white font-medium">
+          Recent Performances
+        </div>
+        <div className="text-center text-neutral-400 pt-3 font-normal ">
+          We have performed in many different stages across India, also
+          conquered levels of awards too
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="/Chargers/chargersrecent.svg"
+            alt="Chargers"
+            height={150}
+            width={669}
+          />
+        </div>
+        <div className="px-10">
+          <div className="w-full h-6 text-white text-xl md:text-xl lg:text-xl font-semibold uppercase leading-normal tracking-wide pb-10">
+            World dance day, vit vellore
+          </div>
 
+          <div className="md:w-96 text-white text-sm sm:text-base font-normal leading-7 text-left justify-center items-center">
+          Chargers stole the spotlight on World Dance Day 2019, transcending the college realm
+           with a performance that echoes far beyond. With over 12 million views on YouTube,
+            their groundbreaking routine sparked a nationwide sensation and has left their 
+            imprint in the hearts of viewers across the world.
+          </div>
+          <div className="py-2"></div>
+          <div className="w-40 h-12 justify-center items-center ">
+            <div className="w-40 h-12 relative bg-white bg-opacity-10 rounded-sm flex-col justify-start items-start flex">
+              <div className="w-40 h-6 absolute">
+                <div className="w-40 h-6 left-0 top-2 absolute text-center text-white text-md md:text-xl font-normal leading-normal tracking-wide ">
+                  Watch Now
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="w-full h-6 px-6 text-white text-xl md:text-xl lg:text-xl font-semibold uppercase leading-normal tracking-wide pt-6 ">
+            international dance day, vit vellore
+          </div>
+          <div className="py-8"></div>
+          <div className="px-4 pl-6 text-white text-sm text-left pb-4">
+            Missed our International dance day performance? Don&apos;t sweat it
+            cause we&apos;ve bought international dance day TO YOU! <br />{" "}
+            Presenting a plethora of performances by our very own dance teams.
+            click below & enjoy the performance.
+          </div>
+          <div className="w-40 h-12  max-sm:px-10 justify-center items-center inline-flex">
+            <div className="w-40 h-12 relative bg-white bg-opacity-10 rounded-sm flex-col justify-start items-start flex">
+              <div className="w-40 h-6 relative">
+                <div className="w-40 h-6 top-2 absolute text-center text-white text-md md:text-xl font-normal leading-normal tracking-wide">
+                  Watch Now
+                </div>
+              </div>
+              <div className="w-5 h-5 origin-top-left rotate-[-34.72deg] bg-gradient-to-b from-emerald-300 to-green-600 rounded-full blur-xl" />
+              <div className="w-6 h-7 origin-top-left rotate-[-34.72deg] bg-gradient-to-b from-pink-400 to-violet-700 rounded-full blur-xl" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Image src="/Chargers/chargersrecent2.svg"
+           height={150} 
+           width={669}
+            alt="Chargers" />
+        </div>
+      </div>
   </div>
   );
 }
