be616faab58908e9c0afb70a48accf9d2bdede2b
anubhav-aryan <aryananubhav08@gmail.com>
2023-10-31 13:19:21 +0530

    fix: alumni gallery settings

--- src/pages/Notablealumni.tsx
+++ src/pages/Notablealumni.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Slideshow: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const numberOfImages = 5;
+  const numberOfImages = 21;
 
   const handlePrevClick = () => {
     setActiveIndex((prevIndex) =>
@@ -43,18 +43,16 @@ const Slideshow: React.FC = () => {
 
   return (
     <>
-      <div className="text-center font-extrabold text-4xl">
-        Notable Alumnis
-      </div>
-      <div className="text-center font-medium text-sm text-gray-400">
+      <div className="text-center font-extrabold text-4xl">Notable Alumnis</div>
+      <div className="text-center font-medium text-sm text-gray-400 pb-10">
         Check out the list of our notable Alumni who are on top of their Passion
       </div>
-      <div className="lg:w-[80vw] mt-[2vh]">
+      <div className="">
         <div className=" mx-auto relative px-[2vw]" data-carousel="static">
           <div className="relative overflow-hidden rounded-lg">
             {imagePaths.map((path, index) => {
               return (
-                <div
+                <div  
                   key={index}
                   className={`duration-700 ease-in-out ${
                     index === activeIndex ? "block" : "hidden"
@@ -65,46 +63,46 @@ const Slideshow: React.FC = () => {
                 >
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[index % 5]} // Use the image path from the array
+                      src={imagePaths[index % 21]} // Use the image path from the array
                       alt={`Image ${index}`}
-                      width={200} // Adjust the width and height as needed
-                      height={200}
+                      width={100} // Adjust the width and height as needed
+                      height={100}
                       className=""
                     />
                   </div>
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[(index + 1) % 5]} // Use the image path from the array
+                      src={imagePaths[(index + 1) % 21]} // Use the image path from the array
                       alt={`Image ${index + 1}`}
-                      width={400} // Adjust the width and height as needed
-                      height={400}
-                      className=""
-                    />
-                  </div>
-                  <div className="flex items-center">
-                    <Image
-                      src={imagePaths[(index + 2) % 5]} // Use the image path from the array
-                      alt={`Image ${index + 2}`}
-                      width={600} // Adjust the width and height as needed
-                      height={600}
-                      className=""
-                    />
-                  </div>
-                  <div className="flex items-center">
-                    <Image
-                      src={imagePaths[(index + 3) % 5]} // Use the image path from the array
-                      alt={`Image ${index + 3}`}
-                      width={400} // Adjust the width and height as needed
-                      height={400}
-                      className=""
-                    />
-                  </div>
-                  <div className="flex items-center">
-                    <Image
-                      src={imagePaths[(index + 4) % 5]} // Use the image path from the array
-                      alt={`Image ${index + 4}`}
                       width={200} // Adjust the width and height as needed
                       height={200}
+                      className=""
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={imagePaths[(index + 2) % 21]} // Use the image path from the array
+                      alt={`Image ${index + 2}`}
+                      width={300} // Adjust the width and height as needed
+                      height={300}
+                      className=""
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={imagePaths[(index + 3) % 21]} // Use the image path from the array
+                      alt={`Image ${index + 3}`}
+                      width={200} // Adjust the width and height as needed
+                      height={200}
+                      className=""
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={imagePaths[(index + 4) % 21]} // Use the image path from the array
+                      alt={`Image ${index + 4}`}
+                      width={100} // Adjust the width and height as needed
+                      height={100}
                       className=""
                     />
                   </div>
@@ -115,33 +113,30 @@ const Slideshow: React.FC = () => {
 
           <button
             type="button"
-            className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            className="absolute top-0 left-60 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-prev
             onClick={handlePrevClick}
           >
             <Image
               src="/chevron-left.svg"
               alt="Left arrow"
-              width={40}
-              height={40}
+              width={50}
+              height={50}
             />
-
-            {/* Left arrow icon */}
           </button>
 
           <button
             type="button"
-            className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            className="absolute top-0 right-60 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-next
             onClick={handleNextClick}
           >
             <Image
               src="/chevron-right.svg"
-              alt="Left arrow"
-              width={40}
-              height={40}
-            />{" "}
-            {/* Right arrow icon */}
+              alt="Right arrow"
+              width={50}
+              height={50}
+            />
           </button>
         </div>
       </div>
