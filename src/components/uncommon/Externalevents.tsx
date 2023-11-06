cd501fcee2047a0334e167e1f252511eb39e360d
anubhav-aryan <aryananubhav08@gmail.com>
2023-11-06 23:36:56 +0530

    fix: external events overflow

--- src/components/uncommon/Externalevents.tsx
+++ src/components/uncommon/Externalevents.tsx
@@ -1,100 +1,82 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Externalevents = () => {
   return (
     <div>
-        <div className=" text-2xl flex py-10 font-semibold">
-        <Image
-                    src="/line.svg"
-                    alt="Board"
-                    width={110}
-                    height={400}
-                  />
-                  Srishti School 2022
-                  <Image
-                    src="/line.svg"
-                    alt="Board"
-                    width={300}
-                    height={300}
-                  />
+      <div className=" text-2xl flex py-10 font-semibold">
+        <Image src="/line.svg" alt="Board" width={110} height={400} />
+        Srishti School 2022
+        <Image src="/line.svg" alt="Board" width={390} height={390} />
       </div>
       <div className=" ">
-      Dance club conducted workshops at Srishti school, near VIT Vellore for the academic years 2022 and 2023.
-      We believe that learning should be a joyful experience. Our fun dance workshop for school kids is
-      designed to do just that! We create an exciting and interactive environment where kids not only learn 
-      the art of dance but also develop confidence, coordination, and teamwork. Through creative movements and 
-      music, our little dancers embark on a journey of self-expression and discovery. Watch your child&apos; face
-      light up as they dance their way to new skills and unforgettable memories
+        Dance club conducted workshops at Srishti school, near VIT Vellore for
+        the academic years 2022 and 2023. We believe that learning should be a
+        joyful experience. Our fun dance workshop for school kids is designed to
+        do just that! We create an exciting and interactive environment where
+        kids not only learn the art of dance but also develop confidence,
+        coordination, and teamwork. Through creative movements and music, our
+        little dancers embark on a journey of self-expression and discovery.
+        Watch your child&apos; face light up as they dance their way to new
+        skills and unforgettable memories
       </div>
       <div className="flex gap-8 py-10">
         <Image
           src="/events/srishti1.svg"
           alt=""
-          height={1000}
-          width={1000}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
 
         <Image
           src="/events/srishti2.svg"
           alt=""
-          height={1000}
-          width={1000}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
 
         <Image
           src="/events/srishti3.svg"
           alt=""
-          height={1000}
-          width={1000}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
       </div>
       <div className=" text-2xl flex py-10 font-semibold">
-        <Image
-                    src="/line.svg"
-                    alt="Board"
-                    width={110}
-                    height={400}
-                  />
-                  Srishti School 2023
-                  <Image
-                    src="/line.svg"
-                    alt="Board"
-                    width={300}
-                    height={300}
-                  />
+        <Image src="/line.svg" alt="Board" width={110} height={400} />
+        Srishti School 2023
+        <Image src="/line.svg" alt="Board" width={390} height={390} />
       </div>
       <div className="flex gap-8 py-10">
         <Image
           src="/events/srishti4.svg"
           alt=""
-          height={1000}
-          width={1000}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
 
         <Image
           src="/events/srishti5.svg"
           alt=""
-          height={1200}
-          width={1200}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
 
         <Image
           src="/events/srishti6.svg"
           alt=""
-          height={1000}
-          width={1000}
-          className="w-full"
+          height={390}
+          width={390}
+          className=""
         />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Externalevents
+export default Externalevents;
--- src/pages/Notablealumni.tsx
+++ src/pages/Notablealumni.tsx
@@ -44,34 +44,33 @@ const Slideshow: React.FC = () => {
 
   return (
     <>
-    <Link
-          href="/"
-          className="absolute hidden lg:block text-white mt-14 pt-2"
+      <Link href="/" className="absolute hidden lg:block text-white mt-14 pt-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-12 h-12"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-12 h-12"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-            />
-          </svg>
-        </Link>
-        <div className="py-10">
-      <div className="text-center font-extrabold text-4xl">Notable Alumnis</div>
-      <div className="text-center font-medium text-sm text-gray-400 pb-10">
-        Check out the list of our notable Alumni who are on top of their Passion
-      </div>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+          />
+        </svg>
+      </Link>
+      <div className="py-10">
+        <div className="text-center font-extrabold text-4xl">
+          Notable Alumnis
+        </div>
+        <div className="text-center font-medium text-sm text-gray-400 pb-10">
+          Check out the list of our notable Alumni who are on top of their Passion
+        </div>
       </div>
       <div className="pb-10">
-        <div className=" mx-auto relative px-[2vw]" data-carousel="static">
-          <div className="relative overflow-hidden rounded-lg">
+        <div className="mx-auto relative px-[2vw]">
+          <div className="relative overflow-hidden rounded-lg" id="slideshow-container">
             {imagePaths.map((path, index) => {
               return (
                 <div
@@ -85,45 +84,45 @@ const Slideshow: React.FC = () => {
                 >
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[index % 21]} // Use the image path from the array
+                      src={imagePaths[index % 21]}
                       alt={`Image ${index}`}
-                      width={150} // Adjust the width and height as needed
+                      width={150}
                       height={150}
                       className=""
                     />
                   </div>
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[(index + 1) % 21]} // Use the image path from the array
+                      src={imagePaths[(index + 1) % 21]}
                       alt={`Image ${index + 1}`}
-                      width={250} // Adjust the width and height as needed
+                      width={250}
                       height={250}
                       className=""
                     />
                   </div>
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[(index + 2) % 21]} // Use the image path from the array
+                      src={imagePaths[(index + 2) % 21]}
                       alt={`Image ${index + 2}`}
-                      width={350} // Adjust the width and height as needed
+                      width={350}
                       height={350}
                       className=""
                     />
                   </div>
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[(index + 3) % 21]} // Use the image path from the array
+                      src={imagePaths[(index + 3) % 21]}
                       alt={`Image ${index + 3}`}
-                      width={250} // Adjust the width and height as needed
+                      width={250}
                       height={250}
                       className=""
                     />
                   </div>
                   <div className="flex items-center">
                     <Image
-                      src={imagePaths[(index + 4) % 21]} // Use the image path from the array
+                      src={imagePaths[(index + 4) % 21]}
                       alt={`Image ${index + 4}`}
-                      width={150} // Adjust the width and height as needed
+                      width={150}
                       height={150}
                       className=""
                     />
@@ -149,7 +148,7 @@ const Slideshow: React.FC = () => {
 
           <button
             type="button"
-            className="absolute top-0 right-60 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none "
+            className="absolute top-0 right-60 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-next
             onClick={handleNextClick}
           >
