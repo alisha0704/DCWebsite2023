bf05b99b1c7631b8f1298c7b4d3654adf1e63432
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-11-06 21:49:10 +0530

    fix: board page

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,137 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import Link from "next/link";
-// import { FiArrowLeft } from "react-icons/fi";
-// import Image from "next/image";
-// import FullpageLoader from "@/components/common/FullpageLoader";
-// import Footer from "@/components/common/Footer";
-
-// type BoardProps = {};
-
-// const Board: React.FC<BoardProps> = () => {
-//   const images = [
-//     {
-//       imageUrl: "/board/aman.svg",
-//       memberName: "Aman Bohara",
-//       memberDesgn: "THE CHAIRPERSON OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/gowri.svg",
-//       memberName: "Gowri Namboodiri",
-//       memberDesgn: "THE VICE-CHAIRPERSON OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/manasa.png",
-//       memberName: "Manasa Madhusoodanan",
-//       memberDesgn: "THE GENERAL SECRETARY OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/sree.svg",
-//       memberName: "Sree Nandhan",
-//       memberDesgn: "THE CO-SECRETARY OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/teerth.svg",
-//       memberName: "Teerth Amin",
-//       memberDesgn: "THE EVENTS HEAD OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/aditi.svg",
-//       memberName: "Aditi Sudhir",
-//       memberDesgn: "THE PUBLICITY HEAD OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/nandin.svg",
-//       memberName: "Nandin Sajith",
-//       memberDesgn: "THE DESIGN HEAD OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/raaga.svg",
-//       memberName: "Raaga Laasya",
-//       memberDesgn: "THE EDITORIAL HEAD OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/srishti.svg",
-//       memberName: "Srishti Sinha",
-//       memberDesgn: "THE FINANCE HEAD OF VIT DANCE CLUB",
-//     },
-//     {
-//       imageUrl: "/board/siraj.svg",
-//       memberName: "Sirajudeen",
-//       memberDesgn: "THE OUTREACH HEAD OF VIT DANCE CLUB",
-//     },
-//   ];
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     setTimeout(() => setLoading(false), 5000);
-//     return () => {
-//       sessionStorage.removeItem("scrollPosition");
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="container">
-//       <Link
-//               href="/"
-//               className="absolute hidden lg:block pt-14 text-white"
-//             >
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke-width="1.5"
-//                 stroke="currentColor"
-//                 className="w-12 h-12"
-//               >
-//                 <path
-//                   stroke-linecap="round"
-//                   stroke-linejoin="round"
-//                   d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-//                 />
-//               </svg>
-//             </Link>
-//         <div className="flex justify-center items-center   bg-black">
-//           <div className="text-center p-8 max-w-screen-lg w-full overflow-y-auto">
-//             <div className="mb-4">
-//               <div className="text-2xl md:text-3xl mb-1 mt-8 text-white">
-//                 The Board
-//               </div>
-//             </div>
-            
-//             <p className="text-sm md:text-md mb-8 text-gray-300">
-//               Alone you can do so little, together you can do so much.
-//               Presenting the Board of Dance Club 2023-24.
-//             </p>
-//             <div className="grid grid-cols-2 md:grid-cols-3 gap-14 py-8 w-full">
-//               {images.map((image, index) => (
-//                 <div key={index} className="text-center">
-//                   <Image
-//                     src={image.imageUrl}
-//                     alt={`Image ${index + 1}`}
-//                     width={1000}
-//                     height={1000}
-//                   />
-//                   <div className="flex flex-col items-center mt-2">
-//                     <h2 className="text-md lg:text-lg font-semibold text-white mt-2">
-//                       {image.memberName}
-//                     </h2>
-//                     <h3 className="text-xs lg:text-sm text-gray-400 mt-1 mb-8">
-//                       {image.memberDesgn}
-//                     </h3>
-//                   </div>
-//                 </div>
-//               ))}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Board;
-
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
@@ -207,7 +73,7 @@ const Board: React.FC<BoardProps> = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="container backdrop-blur-2xl backdrop-brightness-150">
       <Link
               href="/"
               className="absolute hidden lg:block pt-14 text-white"
@@ -227,11 +93,11 @@ const Board: React.FC<BoardProps> = () => {
                 />
               </svg>
             </Link>
-        <div className="flex justify-center items-center bg-black">
+        <div className="flex justify-center items-center ">
           <div className="text-center p-8 max-w-screen-lg w-full overflow-y-auto">
             <div className="mb-4">
-              <div className="text-2xl font-extrabold md:text-3xl mb-1 mt-8 text-white">
-                The Board
+              <div className="text-3xl font-bold md:text-3xl mb-1 mt-8 text-white tracking-wide">
+                THE BOARD
               </div>
             </div>
             <p className="text-sm md:text-md mb-8 text-gray-300">
@@ -284,9 +150,9 @@ const Board: React.FC<BoardProps> = () => {
 
           </div>
         </div>
-      </div>
-     <div className='py-10'>
-      <div className="flex justify-center items-center">
+      
+     <div className='py-10 '>
+      <div className="flex justify-center items-center backdrop-blur-2xl">
       <Image
                     src="/line.svg"
                     alt="Board"
@@ -389,6 +255,7 @@ const Board: React.FC<BoardProps> = () => {
                     width={250}
                     height={250}
                   />
+     </div>
      </div>
      </div>
     </>
