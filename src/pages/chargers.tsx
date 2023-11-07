53ff3dc4c53b1d9642954c82013d2bb6d256dfb0
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-11-07 16:29:50 +0530

    fix: chargers page

--- src/pages/chargers.tsx
+++ src/pages/chargers.tsx
@@ -7,7 +7,9 @@ type chargersProps = {
 
 const chargers:React.FC<chargersProps> = () => {
     
-    return (<div className="xl:w-full lg:w-9/10 w-full mx-auto text-left border-solid border-white px-2 py-2">
+    return (
+      
+    <div className="xl:w-full lg:w-9/10 w-full mx-auto text-left border-solid border-white px-2 py-2 backdrop-blur-2xl">
     <div className="flex max-sm:px-6 justify-center">
             <Image
               src="/Teams/chargerslogo.svg"
@@ -25,7 +27,7 @@ const chargers:React.FC<chargersProps> = () => {
     decade-strong legacy of unstoppable journey.
     </div>
     <hr className="w-3/4 h-0.5 mx-auto my-4 bg-gray-700 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
-    <div className='flex max-sm:px-6 justify-center'>
+    <div className='flex justify-center'>
     <Image
               src="/Teams/chargers.svg"
               alt="chargers"
@@ -34,35 +36,37 @@ const chargers:React.FC<chargersProps> = () => {
             />
     </div>
     <div className="flex justify-center py-10">
-        <div className="px-10 flex items-center ">
+        <div className="md:px-10 flex items-center max-md:hidden">
         <hr className="w-48 h-0.5 mx-auto bg-gray-700 border-0 rounded-sm md:my-2" />
         </div>
         <div className="py-6 text-center justify-center flex border rounded-3xl">
           <Image src="/BBC/Frame 11.svg" alt="BBC" width={40} height={40} className="pl-2" />
-          <div className="px-5  font-bold text-md">
+          <div className="px-5 font-bold text-sm md:text-md">
             The Junior Dance Team of Chargers is{" "}
             <span className="text-[#BBC1FF]"> Energizers </span>
           </div>
         </div>
-        <div className="px-10 flex items-center">
+        <div className="px-10 flex items-center max-md:hidden">
         <hr className="w-48 h-0.5 mx-auto bg-gray-700 border-0 rounded md:my-10" />
         </div>
       </div>
-      <div className="gap-4 columns-3 py-10">
+      <div className="gap-4 columns-1 md:columns-3 py-10">
         <Image
           src="/Chargers/vaishnavi.png"
           alt="chargers"
           width={500}
           height={500}
-          className="pt-12"
+          className="md:pt-12"
         />
         <div className="text-center pt-4 font-semibold">VAISHNAVI P MAHESH</div>
         <div className="text-center text-xs text-gray-400 font-semibold ">
           TEAM REPRESENTATIVE
         </div>
         <Image src="/Chargers/siraj.svg"
-         alt="chargers" width={500} 
-         height={500} />
+         alt="chargers"
+         width={500} 
+         height={500}
+         className="max-md:pt-12" />
         <div className="text-center pt-4 font-semibold">SIRAJUDEEN</div>
         <div className="text-center text-xs text-gray-400 font-semibold">
           TEAM HEAD
@@ -79,7 +83,7 @@ const chargers:React.FC<chargersProps> = () => {
           JDC COORDINATOR - ENERGIZERS
         </div>
       </div>
-      <div className="text-2xl text-center font-extrabold">Mini-Gallery</div>
+      <div className="text-xl md:text-2xl text-center font-extrabold">Mini-Gallery</div>
       <div className="text-center justify-center text-sm text-gray-400 pb-4">
         Glimpse of our latest performances throughout many events
       </div>
@@ -89,6 +93,7 @@ const chargers:React.FC<chargersProps> = () => {
          width={1000} 
          height={1000} />
       </div>
+      <div className='backdrop-blur-2xl backdrop-brightness-150'>
       <div className="lg:px-24 py-10 pb- flex-col flex">
         <div className="text-center text-lg md:text-xl lg:text-2xl text-white font-semibold">
           Recent Performances
@@ -108,13 +113,13 @@ const chargers:React.FC<chargersProps> = () => {
             width={669}
           />
         </div>
-        <div className="px-10">
-          <div className="w-full h-6 text-white text-md md:text-lg font-semibold uppercase leading-normal tracking-wide pb-10">
+        <div className="md:px-10">
+          <div className="w-full h-6 max-sm:text-center text-white text-md md:text-lg font-semibold uppercase leading-normal tracking-wide pb-10">
             World dance day, vit vellore
           </div>
 
           <div className="md:w-96 text-white text-sm sm:text-base font-normal leading-7 text-left justify-center items-center">
-          Chargers stole the spotlight on World Dance Day 2019, transcending the college realm
+           Chargers stole the spotlight on World Dance Day 2019, transcending the college realm
            with a performance that echoes far beyond. With over 12 million views on YouTube,
             their groundbreaking routine sparked a nationwide sensation and has left their 
             imprint in the hearts of viewers across the world.
@@ -131,17 +136,17 @@ const chargers:React.FC<chargersProps> = () => {
           </div>
         </div>
         <div className="">
-          <div className="w-full h-6 px-6 text-white text-md md:text-lg font-semibold uppercase leading-normal tracking-wide pt-6 ">
+          <div className="w-full h-6 max-sm:text-center px-6 text-white text-md md:text-lg font-semibold uppercase leading-normal tracking-wide pt-6 ">
             international dance day, vit vellore
           </div>
           <div className="py-8"></div>
-          <div className="px-4 pl-6 text-white text-sm text-left pb-4">
+          <div className="md:px-4 md:pl-6 text-white text-sm leading-7 text-left pb-4">
             Missed our International dance day performance? Don&apos;t sweat it
             cause we&apos;ve bought international dance day TO YOU! <br />{" "}
             Presenting a plethora of performances by our very own dance teams.
             click below & enjoy the performance.
           </div>
-          <div className="w-40 h-12  max-sm:px-10 justify-center items-center inline-flex">
+          <div className="pl-10 w-40 h-10  max-sm:px-10 justify-center items-center inline-flex">
             <div className="w-40 h-12 relative bg-white bg-opacity-10 rounded-sm flex-col justify-start items-start flex">
               <div className="w-40 h-6 relative">
                 <div className="w-40 h-6 top-2 absolute text-center text-white text-md md:text-lg font-normal leading-normal tracking-wide">
@@ -160,6 +165,7 @@ const chargers:React.FC<chargersProps> = () => {
             alt="Chargers" />
         </div>
       </div>
+  </div>
   </div>
   );
 }
