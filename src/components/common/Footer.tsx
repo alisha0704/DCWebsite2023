a047b71c7b1ee6d352d519b4b24c157a830a170a
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-11-13 17:05:16 +0530

    fix: footer responsive

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -11,15 +11,15 @@ type FooterProps = {};
 const Footer: React.FC<FooterProps> = () => {
   return (
     <div className=" backdrop-blur-3xl gap-y-2 justify-around flex flex-col sm:flex-row border-t-[1px] border-gray-700 pt-12 lg:items-stretch items-stretch">
-      <div className="max-sm:ml-8">
+      <div className="">
         <div
           className={`text-[#A7A9BE] flex flex-col pt-6 text-left text-[1rem] font-semibold gap-1`}
         >
-          <div className="flex flex-col md:items-center">
+          <div className="flex flex-col items-center">
             <Image src="/DClogo.png" alt="DC Logo" width={80} height={80} />
             <text className="text-xl mt-2">VIT DANCE CLUB</text>
           </div>
-
+          <div className="max-sm:ml-8">
           <div className="mt-16 md:ml-10 flex md:items-center">
             <IoLocationOutline size="2em" />
             <div className="ml-2 text-lg">
@@ -38,7 +38,8 @@ const Footer: React.FC<FooterProps> = () => {
             <AiOutlineMail size="2em" />
             <div className="ml-2 text-lg">@vitdcofficial@gmail.com</div>
           </div>
-          <div className="text-[#A7A9BE]  md:text-xl text-base pt-10 pb-20 flex md:items-center ml-10">
+          </div>
+          <div className="text-[#A7A9BE] max-sm:ml-24  md:text-xl text-base pt-10 pb-20 flex md:items-center ml-10">
             <a
               href={"mailto:vitdcofficial@gmail.com"}
               className={`text-black bg-gray-300 font-bold rounded-full text-center sm:px-6 sm:py-5 py-2 px-3 hover:bg-gray-400 hover:text-white transition duration-300 ease-in-out `}
@@ -46,17 +47,18 @@ const Footer: React.FC<FooterProps> = () => {
               Contact us for queries
             </a>
           </div>
+          
         </div>
       </div>
 
-      <div className="flex md:items-center flex-col ml-8 pt-14 mt-2">
+      <div className="flex md:items-center flex-col md:ml-8 pt-14 mt-2">
         <text
-          className={`text-gray-400 font-bold text-[1.2rem] lg:text-[1.5rem] pt-12 gap-6 py-10`}
+          className={`text-gray-400 font-bold text-[1.2rem] text-center lg:text-[1.5rem] pt-12 gap-6 py-10`}
         >
           Information
         </text>
         <div
-          className={`text-[#A7A9BE] text-lg flex flex-col pt-8 text-left text-[1rem] font-semibold gap-1`}
+          className={`text-[#A7A9BE] text-lg flex flex-col pt-8 text-center md:text-left text-[1rem] font-semibold gap-1`}
         >
           <Link href="/" className="py-4 hover:underline">
             Home
@@ -76,14 +78,14 @@ const Footer: React.FC<FooterProps> = () => {
         </div>
       </div>
 
-      <div className="ml-8 py-20 mt-8">
+      <div className="flex md:items-center flex-col md:ml-8 py-10 mt-6">
         <text
-          className={`text-gray-400 font-bold text-[1.2rem] lg:text-[1.5rem] pt-14 gap-6 py-10`}
+          className= {`text-gray-400 font-bold text-[1.2rem] text-center lg:text-[1.5rem] pt-12 gap-6 py-10`}
         >
           Social Media
         </text>
         <div
-          className={`flex flex-col text-left sm:text-right text-[1rem] font-semibold gap-1 pt-8 py-12 text-[#A7A9BE]`}
+          className={`flex flex-col text-center md:text-left items-center text-[1rem] font-semibold gap-1 pt-8 py-12 text-[#A7A9BE]`}
         >
           <a
             className="py-3"
