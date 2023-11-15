0d5823a738cd01462ae11d13e64979cf56883608
anubhav-aryan <aryananubhav08@gmail.com>
2023-11-15 11:44:40 +0530

    fix: header

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -29,10 +29,11 @@ const Layout = ({ children }: LayoutProps) => {
       <Rightsidebar />
       <Header />
       <main className="flex flex-row text-white justify-center items-center backdrop-blur-xl">
-        <div className="max-sm:px-4 max-sm:backdrop-blur-2xl w-full md:w-8/12 ">{children}</div>
+        <div className="max-sm:px-4 max-sm:backdrop-blur-2xl w-full md:w-8/12 ">
+          {children}
+        </div>
       </main>
       <Footer />
-
     </>
   );
 };
--- src/components/common/Leftsidebar.tsx
+++ src/components/common/Leftsidebar.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Leftsidebar = () => {
   return (
     <div className="bg-black fixed h-full w-0 md:w-48 opacity-70">
-      <div className="h-screen flex flex-col justify-between">
-      </div>
+      <div className="h-screen flex flex-col justify-between"></div>
     </div>
   );
 };
